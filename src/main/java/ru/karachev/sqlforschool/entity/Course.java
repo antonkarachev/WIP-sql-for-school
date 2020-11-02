6e4f402c138caa577732cb58b43e201d15ad9102
antonkarachev <antonkarachev@gmail.com>
2020-11-02 12:03:21 +0300

    final corrections

--- src/main/java/ru/karachev/sqlforschool/entity/Course.java
+++ src/main/java/ru/karachev/sqlforschool/entity/Course.java
@@ -6,29 +6,29 @@ public class Course {
     private final Integer courseId;
     private final String courseName;
     private final String description;
-
+    
     private Course(Builder builder) {
         this.courseId = builder.courseId;
         this.courseName = builder.courseName;
         this.description = builder.description;
     }
-
+    
     public static Builder builder() {
         return new Builder();
     }
-
+    
     public Integer getId() {
         return courseId;
     }
-
+    
     public String getName() {
         return courseName;
     }
-
+    
     public String getDescription() {
         return description;
     }
-
+    
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -42,12 +42,12 @@ public class Course {
                 Objects.equals(courseName, course.courseName) &&
                 Objects.equals(description, course.description);
     }
-
+    
     @Override
     public int hashCode() {
         return Objects.hash(courseId, courseName, description);
     }
-
+    
     @Override
     public String toString() {
         return "Course{" +
@@ -56,29 +56,30 @@ public class Course {
                 ", description='" + description + '\'' +
                 '}';
     }
-
+    
     public static class Builder {
         private Integer courseId;
         private String courseName;
         private String description;
-
+        
         public Builder withCourseId(Integer courseId) {
             this.courseId = courseId;
             return this;
         }
-
+        
         public Builder withCourseName(String courseName) {
             this.courseName = courseName;
             return this;
         }
-
+        
         public Builder withDescription(String description) {
             this.description = description;
             return this;
         }
-
+        
         public Course build() {
             return new Course(this);
         }
     }
 }
+
